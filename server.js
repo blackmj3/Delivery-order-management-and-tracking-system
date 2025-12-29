b2ba5d4dbd5a582e4c5b45877b8c22d8faabace6
shafiqa ghosn <shafika.gh97@gmail.com>
2025-12-29 14:07:34 +0400

    create http server + socket server

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const http = require("http");
+const { Server } = require("socket.io");
+//http server
+const server = http.createServer(app);
+//socket server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+// init socket logic
+
+const PORT = process.env.PORT || 3000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Connect with database done");
+
+    server.listen(PORT, () => {
+      console.log("Server + Socket running on port ", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
