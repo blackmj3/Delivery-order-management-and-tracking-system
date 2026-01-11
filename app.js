395d3ed72452a9749440faf339f4b5345491c65c
Raad Raad <raad20011127@gmail.com>
2026-01-11 21:52:31 +0300

    WIP

--- app.js
+++ app.js
@@ -1,58 +1,21 @@
 // app.js
-const express = require("express");
-const authRoutes = require("./src/routes/authRoutes");
-
-
-const cookieParser = require('cookie-parser');
-
-const app = express();
-const cookies = require("cookie-parser");
-const { apiLimiter } = require("./src/middlewares/limiter");
-
-const helmet = require("helmet");
-//const xssSanitize = require("./middlewares/xss");
-
+const express = require("express") ;
+const app = express() ;
 app.use(express.json())
-app.use(cookies())
+const path = require("path");
+
+const xssSanitize = require("./src/middlewares/xssMiddleware");
 
 // protect from xss
-//app.use(xssSanitize);
+app.use(xssSanitize);
 
-// Enhanced security headers specifically for auth
-app.use(helmet({
-    contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            /* styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"], */
-            scriptSrc: ["'self'", "'unsafe-inline'"],
-            imgSrc: ["'self'", "data:", "https:"],
-            /* connectSrc: ["'self'", "https://api.yourdomain.com"],
-            fontSrc: ["'self'", "https://fonts.gstatic.com"], */
-            frameAncestors: ["'none'"], // Prevent clickjacking
-            formAction: ["'self'"] // Restrict form submissions
-        },
-    },
-    crossOriginEmbedderPolicy: false,
-    hsts: {
-        maxAge: 31536000, // 1 year
-        includeSubDomains: true,
-        preload: true
-    },
-    referrerPolicy: { policy: 'strict-origin-when-cross-origin' }
-}));
-// Rate Limiter
-app.use(apiLimiter);
+app.use(express.urlencoded({extended : true}));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // User Apis
 app.use("/users" , require("./src/routes/userRoute"));
 
 // Rate Apis 
-app.use("/Rating" , require("./src/routes/rateRoute"));
+app.use("/Rating" , require("./src/routes/rateRoute")) ;
 
-app.use("/api/v1/orders", require("./src/routes/orders.routes"));
-// Error Middleware
-app.use(require("./src/middlewares/errorMiddleware"));
-
-// Not Found
-//app.use(require("./middlewares/notFound"));
 module.exports = app;
--- src/middlewares/notFoundMiddleware.js
+++
@@ -1,6 +0,0 @@
-const notFound = (req, res) => res.status(404).json({
-    success: false,
-    message: "Not Found"
-})
-
-module.exports = notFound
