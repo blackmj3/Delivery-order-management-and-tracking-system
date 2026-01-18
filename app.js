fdff9df415f9dc78e684b0fc7f1e9a90ea922688
Majd hosaino <blackmj123@gmail.com>
2026-01-18 19:40:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,49 +6,18 @@ const authRoutes = require("./src/routes/authRoutes");
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const cookies = require("cookie-parser");
-const { apiLimiter } = require("./src/middlewares/limiter");
+app.use(express.json());
+app.use(cookieParser());
 
-const helmet = require("helmet");
-//const xssSanitize = require("./middlewares/xss");
+//
+app.use("/api/auth", authRoutes);
 
-app.use(express.json())
-app.use(cookies())
+//
 
-// protect from xss
-//app.use(xssSanitize);
+// app.post("/api/auth/register", (req, res) => {
+//   res.json({ ok: true, body: req.body });
+// });
 
-// Enhanced security headers specifically for auth
-app.use(helmet({
-    contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            /* styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"], */
-            scriptSrc: ["'self'", "'unsafe-inline'"],
-            imgSrc: ["'self'", "data:", "https:"],
-            /* connectSrc: ["'self'", "https://api.yourdomain.com"],
-            fontSrc: ["'self'", "https://fonts.gstatic.com"], */
-            frameAncestors: ["'none'"], // Prevent clickjacking
-            formAction: ["'self'"] // Restrict form submissions
-        },
-    },
-    crossOriginEmbedderPolicy: false,
-    hsts: {
-        maxAge: 31536000, // 1 year
-        includeSubDomains: true,
-        preload: true
-    },
-    referrerPolicy: { policy: 'strict-origin-when-cross-origin' }
-}));
-// Rate Limiter
-app.use(apiLimiter);
+app.get("/", (req, res) => res.send("Hello world"));
 
-//app.use("/api/v1/orders", require("./src/routes/orders.routes"));
-app.use("/api/v1/admin", require("./src/routes/admin.route"));
-app.use("/api/v1/auth", require("./src/routes/authRoutes"));
-// Error Middleware
-app.use(require("./src/middlewares/errorMiddleware"));
-
-// Not Found
-//app.use(require("./middlewares/notFound"));
 module.exports = app;
